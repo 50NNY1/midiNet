cd5ca5efdf7b131225aeb6ff0a3c0d2d02ff3bd1
Sonny <sonnysonic@gmail.com>
2022-12-19 15:25:29 +0000

    audio working. conda env solved. not implemented inline with tkinter

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import tkinter as tk
 from tkinter import filedialog, Text
-import os
+from playsound import playsound
+from functools import partial
 import numpy
 import pyaudio
+import os
+
+# import sound files and load into unique functions
+sounds = []
+for filename in os.listdir('key_sounds'):
+    if filename != '.DS_Store':
+        f = os.path.join('key_sounds', filename)
+        if os.path.isfile(f):
+            sounds.append(f)
+
+
+# play note function
+
 root = tk.Tk()
 root.mainloop()
-pa = pyaudio.PyAudio()
-
-pa = pyaudio.PyAudio()
-strm = pa.open(
-    format=pyaudio.paInt16,
-    channels=2,
-    rate=44100,
-    output=True,
-)
-s = []
-
-sounds = {}
-for filename in os.listdir('key sounds')(13):
-    f = os.path.join('key sounds', filename)
-    if os.path.isfile(f):
-        print(f)
+playsound(sounds[0])
+playsound(sounds[3])
+playsound(sounds[7])
