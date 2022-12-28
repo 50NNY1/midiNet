d89b328f2dd77d543d3ffee7c85febbaf8e5578c
Sonny <sonnysonic@gmail.com>
2022-12-28 17:33:32 +0000

    fixed button issues with toggling

--- app.py
+++ app.py
@@ -16,36 +16,38 @@ def playnote(url):
     sound = AudioSegment.from_wav(url)
     play(sound)
 
-
+#notes array, for labelling buttons
 notes = ["c3", "c#3", "d3", "d#3", "e3", "f3",
          "f#3", "g3", "g#3", "a3", "a#3", "b3", "c4"]
-
 
 # dict of various note functions (must be initalised as so, cannot pass arguements through button command)
 notefuncs = {f'note{i}': partial(
     playnote, url=sounds[i]) for i in range(len(sounds))}
+
+#dict to store buttons and button states (on or off)
+state = {'buttons': [], 'button_values': [
+    [False for i in range(8)] for j in range(len(sounds))]}
 
 # build gui
 root = tk.Tk()
 on = PhotoImage(file="images/on.png")
 off = PhotoImage(file="images/off.png")
 
-state = {'buttons': [], 'button_values': [
-    [False for i in range(8)] for j in range(len(sounds))]}
-
-
+#button constructor (without classes, as we are using a dict to manage instances)
 def create_button(row, col):
     def toggle_button():
-        if state['button_values'][row][col]:
+        if state['button_values'][col][row]:
             state['buttons'][col][row].config(image=off) 
+            state['button_values'][col][row] = False 
         else:
             notefuncs[f'note{row}']()
             state['buttons'][col][row].config(image=on)
+            state['button_values'][col][row] = True
     return tk.Button(root, text=notes[row],
                      command=toggle_button, bg='white', 
                      image=off, compound=LEFT, bd=0)
 
-
+#create grid of buttons
 for i in range(8):
     button_row = []
     for j in range(len(sounds)):
@@ -53,8 +55,32 @@ for i in range(8):
         button.grid(row=j, column=i)
         button_row.append(button)
     state['buttons'].append(button_row)
-   # buttons.append(tk.Button(root, text=notes[j], command=notefuncs[f'note{j}']).grid(
-   #    row=j, column=i))
+    
+#sequencer parameters
+#param_col = len(sounds)+1
+#bpm_label = Label(root, text="BPM")
+#bpm_label.grid(row=0, column=param_col)
+#bpm_box = Entry(root)
+#bpm_box.grid(row=1, column=param_col)
+#play = Button(root, text="play")
+#play.grid(row=2, column=param_col)
+#stop = Button(root, text="stop")
+#stop.grid(row=3, column=param_col)
+#
+##bit of extra code required for the NN button as it needs to be toggleable
+#global nn_state
+#nn_state = False
+#def nn_toggle():
+#    if not nn_state: 
+#        nn.config(image=on)
+#        nn_state = True
+#        #run predict here
+#    elif nn_state:
+#        nn.config(image=off)
+#        nn_state = False
+#nn = Button(root, text="neural net", command=nn_toggle, 
+#            bg="white", image=off, compound=LEFT, bd=0)
+#nn.grid(row=4, column=param_col)
 
-
+#render graphics             
 root.mainloop()
